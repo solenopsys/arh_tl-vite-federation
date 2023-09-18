e90d4355f6f2b1032f10bc20e6d39aa4efbbcfa3
AlexStorm <info@alexstorm.ru>
2023-09-18 21:50:19 +0530

    refactoring

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,9 @@ import {Plugin} from 'vite';
 import {TsCompilerPlugin} from "./ts-compile.plugin";
 
 
-export function angular(injectStubs:string[] ): Plugin[] {
-
-    console.info("Inject stubs", injectStubs)
-
-    return [TsCompilerPlugin]
+export function angular(injectStubs: string[]): Plugin[] {
+    console.info("Inject stubs", injectStubs);
+    return [TsCompilerPlugin];
 }
 
-export default angular;
+export { angular as default };
